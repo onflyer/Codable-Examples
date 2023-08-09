5816eece030ccb87527be5816ace5e99bbfdff40
onflyer <salkaorama@gmail.com>
2023-08-09 14:23:46 +0200

    Initial Commit

---
+++ Codable Examples/Codable_ExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Codable_ExamplesApp.swift
+//  Codable Examples
+//
+//  Created by Aleksandar Milidrag on 8/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Codable_ExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
